589e52107b62aeddc150b9e121b7cd258ad18877
opensvn <justleefeng@gmail.com>
2023-01-11 14:04:48 +0800

    refactor: remove code indentation

--- src/main.rs
+++ src/main.rs
@@ -46,21 +46,23 @@ fn main() {
         .filter(|e| !e.file_type().is_dir()) {
         let name = String::from(entry.file_name().to_string_lossy());
         for publisher in &publishers {
-            if name.starts_with(publisher) {
-                let mut new_name = name.replace(publisher, "");
-                let index = new_name.rfind(".").unwrap_or_else(|| {
-                    panic!("Can not find the right most dot")
-                });
-                new_name.insert_str(index + 1, publisher);
-
-                let parent = entry.path().parent().unwrap_or_else(|| {
-                    panic!("Get parent directory error")
-                });
-                fs::rename(entry.path(), parent.join(new_name)).unwrap_or_else(|error| {
-                    panic!("Problem renaming the file: {:?}", error);
-                });
-                break;
+            if !name.starts_with(publisher) {
+                continue;
             }
+
+            let mut new_name = name.replace(publisher, "");
+            let index = new_name.rfind(".").unwrap_or_else(|| {
+                panic!("Can not find the right most dot")
+            });
+            new_name.insert_str(index + 1, publisher);
+
+            let parent = entry.path().parent().unwrap_or_else(|| {
+                panic!("Get parent directory error")
+            });
+            fs::rename(entry.path(), parent.join(new_name)).unwrap_or_else(|error| {
+                panic!("Problem renaming the file: {:?}", error);
+            });
+            break;
         }
     }
 }
